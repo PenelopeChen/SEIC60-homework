f0c9e72f51668b1b4ff2f5251fb5c41ba9ed3830
Penelope Chen <pen.c828@gmail.com>
2022-12-06 16:45:43 +1100

    3rd exercise done

--- 10-Penelope-Chen/wk01/2-tue/script.js
+++ 10-Penelope-Chen/wk01/2-tue/script.js
@@ -5,9 +5,21 @@
 // }
 
 //Section 2 
-const a = 68
-if (a % 2 === 0) {
-    console.log("After running our calculations, the result is even")
-} else { 
-    console.log("After running out calculations, the result is odd")
+// const a = 68
+// if (a % 2 === 0) {
+//     console.log("After running our calculations, the result is even")
+// } else { 
+//     console.log("After running out calculations, the result is odd")
+// }
+
+//Section 3 
+const b = 75
+if (b <= 25) {
+    console.log("bottom quartile")
+} else if (b > 25 && b <= 50) {
+    console.log("lower quartile")
+} else if (b > 50 && b <= 75) {
+    console.log("upper quartile")
+} else if (b > 75 && b <= 100) {
+    console.log("top quartile")
 }
