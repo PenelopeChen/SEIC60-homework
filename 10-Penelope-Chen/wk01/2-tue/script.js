e4217335d41c076064cc7457e555a15012fe9604
Penelope Chen <pen.c828@gmail.com>
2022-12-06 16:41:23 +1100

    2 exercises done

---
+++ 10-Penelope-Chen/wk01/2-tue/script.js
@@ -0,0 +1,13 @@
+//Section 1
+// const age = 24
+// if (age <= 19) {
+//     console.log("You're still a teenager!")
+// }
+
+//Section 2 
+const a = 68
+if (a % 2 === 0) {
+    console.log("After running our calculations, the result is even")
+} else { 
+    console.log("After running out calculations, the result is odd")
+}
