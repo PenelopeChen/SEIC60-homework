2d246d91d549037da3bb558611508dce1c443c78
Penelope Chen <pen.c828@gmail.com>
2022-12-07 21:40:32 +1100

    attempted 3rd task but wut

--- 10-Penelope-Chen/wk01/3-wed/functions.js
+++ 10-Penelope-Chen/wk01/3-wed/functions.js
@@ -13,24 +13,30 @@
 // console.log(calculate(4, 2, "exp"))
 
 // helloIn 
-const helloIn = function (lang) {
-    // copied Fung's code but didn't remember it correctly as his was formatted in straight line, only just realised it was an object oop
-    const langName = {
-        English: "Hello", 
-        German: "Hallo", 
-        Japanese: "Konnichiwa", 
-        French: "Bonjour"  
-    }
-    return langName 
-    if (langName === true) {
-        console.log(`Sorry I don't speak ${lang}`)
-    }
-}
-helloIn("English")
-helloIn("German")
-helloIn("Japanese")
-helloIn("French")
-helloIn("Latin")
+// const helloIn = function (lang) {
+//     // copied Fung's code but didn't remember it correctly as his was formatted in straight line, only just realised it was an object oop
+//     const langName = {
+//         English: "Hello", 
+//         German: "Hallo", 
+//         Japanese: "Konnichiwa", 
+//         French: "Bonjour"  
+//     }
+//     return langName 
+//     // I know I'm doing something wrong here...
+//     if (langName = true) {
+//         console.log(`Sorry I don't speak ${lang}`)
+//     }
+// }
+// helloIn("English")
+// helloIn("German")
+// helloIn("Japanese")
+// helloIn("French")
+// helloIn("Latin")
 // nothing in console, I'll figure this out in the morning
 
-// 
+// leapYears 
+// const leapYears = function () {
+//     for (let i = 1924; i < 26; i+=4)
+//     console.log(i)
+// }
+// leapYears()
