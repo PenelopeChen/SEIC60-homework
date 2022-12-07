4b1e449d3c655317f0864dfecee7568186c63fef
Penelope Chen <pen.c828@gmail.com>
2022-12-07 17:02:33 +1100

    gonna fix at home

--- 10-Penelope-Chen/wk01/3-wed/functions.js
+++ 10-Penelope-Chen/wk01/3-wed/functions.js
@@ -1,12 +1,20 @@
-const calculate = function (num1, num2, operation) {
-    if (operation === "add") {
-        return (num1 + num2)
-    } else if (operation === "sub") { 
-        return (num1 - num2)
-    }
-}
-console.log(calculate(4, 2, "add"))
-console.log(calculate(4, 2, "sub"))
+// calculate
+// const calculate = function (num1, num2, operation) {
+//     if (operation === "add") {
+//         return (num1 + num2)
+//     } else if (operation === "sub") { 
+//         return (num1 - num2)
+//     }
+// }
+// console.log(calculate(4, 2, "add"))
+// console.log(calculate(4, 2, "sub"))
 // console.log(calculate(4, 2, "mult"))
 // console.log(calculate(4, 2, "div"))
 // console.log(calculate(4, 2, "exp"))
+
+// helloIn 
+const helloIn = function (lang) {
+    const inputLang = (`${}`)
+    return "konnichiwa"
+}
+helloIn(Japanese)
