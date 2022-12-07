937314057d433cf7bc4342644f362610c03a8c95
Penelope Chen <pen.c828@gmail.com>
2022-12-07 21:28:24 +1100

    attempted 2nd task

--- 10-Penelope-Chen/wk01/3-wed/functions.js
+++ 10-Penelope-Chen/wk01/3-wed/functions.js
@@ -14,7 +14,23 @@
 
 // helloIn 
 const helloIn = function (lang) {
-    const inputLang = (`${}`)
-    return "konnichiwa"
+    // copied Fung's code but didn't remember it correctly as his was formatted in straight line, only just realised it was an object oop
+    const langName = {
+        English: "Hello", 
+        German: "Hallo", 
+        Japanese: "Konnichiwa", 
+        French: "Bonjour"  
+    }
+    return langName 
+    if (langName === true) {
+        console.log(`Sorry I don't speak ${lang}`)
+    }
 }
-helloIn(Japanese)
+helloIn("English")
+helloIn("German")
+helloIn("Japanese")
+helloIn("French")
+helloIn("Latin")
+// nothing in console, I'll figure this out in the morning
+
+// 
