07f02efac92d129a2396209fb194f4414ac7a02c
Penelope Chen <pen.c828@gmail.com>
2022-12-08 20:54:49 +1100

    think the 1st task is done

---
+++ 10-Penelope-Chen/wk01/4-thu/script.js
@@ -0,0 +1,7 @@
+// Where's Waldo
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                      ["Lucinda", "Jacc", "Neff", "Snoop"],
+                      ["Petunia", ["Baked Goods", "Waldo"]]]
+whereIsWaldo.splice(1, 1)
+whereIsWaldo[2][2] = "No One"
+console.log("Waldo")
