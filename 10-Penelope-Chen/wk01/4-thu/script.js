88ca414090a8ed42110a60c65036fbe3b1538a06
Penelope Chen <pen.c828@gmail.com>
2022-12-08 22:09:59 +1100

    2nd task done

--- 10-Penelope-Chen/wk01/4-thu/script.js
+++ 10-Penelope-Chen/wk01/4-thu/script.js
@@ -1,7 +1,31 @@
 // Where's Waldo
-const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-                      ["Lucinda", "Jacc", "Neff", "Snoop"],
-                      ["Petunia", ["Baked Goods", "Waldo"]]]
-whereIsWaldo.splice(1, 1)
-whereIsWaldo[2][2] = "No One"
-console.log("Waldo")
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+//                       ["Lucinda", "Jacc", "Neff", "Snoop"],
+//                       ["Petunia", ["Baked Goods", "Waldo"]]]
+// whereIsWaldo.splice(1, 1)
+// whereIsWaldo[2][2] = "No One"
+// console.log(whereIsWaldo(2, 1, 1))
+
+// Array Restaurant
+// let items = [
+//     "chicken",
+//     "apple pie",
+//     "soup",
+//     "steak",
+//     "salad",
+//     "turkey sandwich",
+//     "potato bake"
+// ]
+// console.log(items.sort())
+// items.shift()
+// console.log(items)
+// items.splice(2, 1)
+// console.log(items)
+// const request = prompt("What would you like to eat?")
+// if (request === "chicken" && items[0] === "chicken") {
+//     console.log("Sure, the chicken will be served in 15 minutes")
+// } else {
+//     console.log("Sorry, we don't have that.")
+// }
+
+// The Solar System, Part 1
