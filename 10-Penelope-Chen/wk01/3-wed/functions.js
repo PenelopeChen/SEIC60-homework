77b0edf157845ecb8e20adc9669de1ad5e11a339
Penelope Chen <pen.c828@gmail.com>
2022-12-26 14:53:48 +1100

    fixed hw

--- 10-Penelope-Chen/wk01/3-wed/functions.js
+++ 10-Penelope-Chen/wk01/3-wed/functions.js
@@ -4,6 +4,12 @@
 //         return (num1 + num2)
 //     } else if (operation === "sub") { 
 //         return (num1 - num2)
+//     } else if (operation === "mult") {
+//         return (num1 * num2)
+//     } else if (operation === "div") {
+//         return (num1 / num2)
+//     } else if (operation === "exp") {
+//         return (num1 ** num2)
 //     }
 // }
 // console.log(calculate(4, 2, "add"))
@@ -14,29 +20,27 @@
 
 // helloIn 
 // const helloIn = function (lang) {
-//     // copied Fung's code but didn't remember it correctly as his was formatted in straight line, only just realised it was an object oop
-//     const langName = {
-//         English: "Hello", 
-//         German: "Hallo", 
-//         Japanese: "Konnichiwa", 
-//         French: "Bonjour"  
-//     }
-//     return langName 
-//     // I know I'm doing something wrong here...
-//     if (langName = true) {
-//         console.log(`Sorry I don't speak ${lang}`)
+//     if (lang === "English") {
+//         return `Hello`
+//     } else if (lang === "German") {
+//         return `Hallo`
+//     } else if (lang === "Japanese") {
+//         return `Konnichiwa`
+//     } else if (lang === "French") {
+//         return `Bonjour`
+//     } else {
+//         return `Sorry I don't speak ${lang}`
 //     }
 // }
-// helloIn("English")
-// helloIn("German")
-// helloIn("Japanese")
-// helloIn("French")
-// helloIn("Latin")
-// nothing in console, I'll figure this out in the morning
+// console.log(helloIn("English"))
+// console.log(helloIn("German"))
+// console.log(helloIn("Japanese"))
+// console.log(helloIn("French"))
+// console.log(helloIn("Latin"))
 
 // leapYears 
 // const leapYears = function () {
-//     for (let i = 1924; i < 26; i+=4)
+//     for (let i = 1924; i < 2024; i+=4)
 //     console.log(i)
 // }
 // leapYears()
