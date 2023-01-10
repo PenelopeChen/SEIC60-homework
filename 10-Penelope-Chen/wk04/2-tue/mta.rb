79fb9f171cf25e79206d6a0084f901afcd34da62
Penelope Chen <pen.c828@gmail.com>
2023-01-10 22:10:15 +1100

    found best approach b4 I get sleepy

--- 10-Penelope-Chen/wk04/2-tue/mta.rb
+++ 10-Penelope-Chen/wk04/2-tue/mta.rb
@@ -12,9 +12,9 @@
 # --> Start here =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # (I guess I'm hard coding this 1st)
-# Trying out 2 different ways actually 
+# Trying out 3 different ways actually 
 
-# Ruby arrays for each train line
+# Ruby arrays for each train line (syntax is off tho)
 
 # n_line = %w( TimesSquare 34th 28thN 23rdN UnionSquare 8thN )
 # l_line = %w( 8thL 6th UnionSquare 3rd 1st )
@@ -30,7 +30,7 @@
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# Ruby hash (might be better in this case)
+# Ruby hash (best approach imo)
 # lines = {
 #   "N Line" => {
 #     0 => "Times Square",
@@ -58,7 +58,7 @@
 # }
 
 # Ruby function for hash
-# def plan_trip 
+# def plan_trip 'N', 'Times Square', '6', '33rd'
     # puts "You must travel through the following stops on the #{lines}: #{lines["N Line"][1]}, 28thN, 23rdN, Union Square."
     # puts "Change at #{lines["N Line"].last}."
     # puts "Your journey continues through the following stops: 23rd6, 28th6, 33rd."
@@ -69,11 +69,15 @@
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-# Ruby array of array
+# Ruby array of array (funny syntax again)
 
-lines = [
-  "N Line" ["Times Square", "34th", "28thN", "23rdN", "Union Square", "8thN"],
-  "L Line" ["8thL", "6th", "Union Square", "3rd", "1st"],
-  "6 Line" ["Grand Central", "33rd", "28th6", "23rd6", "Union Square", "Astor Place"],
-]
+# lines = [
+#     %w ( TimesSquare 34th 28thN 23rdN UnionSquare 8thN),
+#     %w ( 8thL 6th Union Square 3rd 1st ),
+#     %w ( Grand Central 33rd 28th6 23rd6 Union Square AstorPlace),
+# ]
 
+# puts "You must travel through the following stops on the #{lines[0]}: #{lines[0][1]}, #{lines[0][2]}, #{lines[0][3]}, #{lines[0][4]}."
+# puts "Change at #{lines[0][4]}."
+# puts "Your journey continues through the following stops: #{lines[2][3]}, #{lines[2][2]}, #{lines[2][1]}."
+# puts "#{} stops in total."
