3b69b4e314887be472e4b896793beb66761cad85
Penelope Chen <pen.c828@gmail.com>
2023-01-11 21:13:32 +1100

    not working...

--- 10-Penelope-Chen/wk04/3-wed/books-one/main.rb
+++ 10-Penelope-Chen/wk04/3-wed/books-one/main.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty' 
 
+# root path/home page
 get '/' do 
     erb :home
 end
 
 get '/info' do
     # get the book info
-    # @info = #StockQuote::.quote params[:stock_code]
+    title = “Jaws” # IRL: this will come from params 
+    book_url = "https://www.googleapis.com/books/v1/volumes?q=title:#{ title }" 
+    @info = HTTParty.get book_url 
+    @info[“items”][0][“volumeInfo”][“imageLinks”][“thumbnail”]
     # render the result
     erb :info
 end
