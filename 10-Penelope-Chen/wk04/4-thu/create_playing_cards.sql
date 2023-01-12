4aefb4b9997cd7717f1b84d7a9387515234ca1f6
Penelope Chen <pen.c828@gmail.com>
2023-01-12 20:26:13 +1100

    fixed database.sqlite3 & done most of sql file except image urls

---
+++ 10-Penelope-Chen/wk04/4-thu/create_playing_cards.sql
@@ -0,0 +1,60 @@
+CREATE TABLE playing_cards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    suit TEXT, 
+    power INTEGER, 
+    image TEXT 
+);
+
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (1, "Ace of Clubs", "clubs", 1, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (2 , "Two of Clubs", "clubs", 2, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (3 , "Three of Clubs", "clubs", 3, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (4 , "Four of Clubs", "clubs", 4, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (5 , "Five of Clubs", "clubs", 5, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (6 , "Six of Clubs", "clubs", 6, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (7 , "Seven of Clubs", "clubs", 7, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (8 , "Eight of Clubs", "clubs", 8, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (9 , "Nine of Clubs", "clubs", 9, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (10 , "Ten of Clubs", "clubs", 10, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (11 , "Jack of Clubs", "clubs", 11, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (12 , "Queen of Clubs", "clubs", 12, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (13 , "King of Clubs", "clubs", 13, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (14 , "Ace of Diamonds", "diamonds", 1, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (15 , "Two of Diamonds", "diamonds", 2, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (16 , "Three of Diamonds", "diamonds", 3, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (17 , "Four of Diamonds", "diamonds", 4, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (18 , "Five of Diamonds", "diamonds", 5, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (19 , "Six of Diamonds", "diamonds", 6, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (20 , "Seven of Diamonds", "diamonds", 7, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (21 , "Eight of Diamonds", "diamonds", 8, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (22 , "Nine of Diamonds", "diamonds", 9, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (23 , "Ten of Diamonds", "diamonds", 10, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (24 , "Jack of Diamonds", "diamonds", 11, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (25 , "Queen of Diamonds", "diamonds", 12, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (26 , "King of Diamonds", "diamonds", 13);
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (27 , "Ace of Hearts", "hearts", 1, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (28 , "Two of Hearts", "hearts", 2, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (29 , "Three of Hearts", "hearts", 3, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (30 , "Four of Hearts", "hearts", 4, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (31 , "Five of Hearts", "hearts", 5, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (32 , "Six of Hearts", "hearts", 6, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (33 , "Seven of Hearts", "hearts", 7, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (34 , "Eight of Hearts", "hearts", 8, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (35 , "Nine of Hearts", "hearts", 9, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (36 , "Ten of Hearts", "hearts", 10, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (37 , "Jack of Hearts", "hearts", 11, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (38 , "Queen of Hearts", "hearts", 12, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (39 , "King of Hearts", "hearts", 13, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (40 , "Ace of Spades", "spades", 1, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (41 , "Two of Spades", "spades", 2, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (42 , "Three of Spades", "spades", 3, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (43 , "Four of Spades", "spades", 4, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (44 , "Five of Spades", "spades", 5, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (45 , "Six of Spades", "spades", 6, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (46 , "Seven of Spades", "spades", 7, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (47 , "Eight of Spades", "spades", 8, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (48 , "Nine of Spades", "spades", 9, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (49 , "Ten of Spades", "spades", 10, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (50 , "Jack of Spades", "spades", 11, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (51 , "Queen of Spades", "spades", 12, );
+INSERT INTO playing_cards (id, name, suit, power, image) VALUES (52 , "King of Spades", "spades", 13, );
---
+++ 10-Penelope-Chen/wk04/4-thu/main.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+get '/' do 
+    erb :home 
+end 
+
+# INDEX 
+
+# NEW 
+
+# CREATE *
+
+# SHOW 
+
+# EDIT 
+
+# POST
+
+# UPDATE *
+
+# DELETE *
