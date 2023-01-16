09e3d77909b62ef0ecb34cbdeb62f415943734dd
Penelope Chen <pen.c828@gmail.com>
2023-01-16 20:17:54 +1100

    routes.rb done

--- 10-Penelope-Chen/wk05/1-mon/games_on_rails/config/routes.rb
+++ 10-Penelope-Chen/wk05/1-mon/games_on_rails/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'pages#home'
+
+  get '/magic' => 'magic#welcome'
+  get '/magic/game' => 'magic#game'
+
+  get '/number' => 'number#welcome'
+  get '/number/game' => 'number#game'
+
+  get '/rps' => 'rps#welcome'
+  get '/rps/game' => 'rps#game'
 end
