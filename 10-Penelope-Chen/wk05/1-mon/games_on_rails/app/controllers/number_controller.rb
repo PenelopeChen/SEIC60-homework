2b1de4276a432fa45e2968e2737a2b105612cc0a
Penelope Chen <pen.c828@gmail.com>
2023-01-16 22:32:12 +1100

    magic works but still figuring out secret no. I gotta sleep.

--- 10-Penelope-Chen/wk05/1-mon/games_on_rails/app/controllers/number_controller.rb
+++ 10-Penelope-Chen/wk05/1-mon/games_on_rails/app/controllers/number_controller.rb
@@ -3,5 +3,23 @@ class NumberController < ApplicationController
     end
 
     def game
+        ai = Random.rand 1..10
+
+        if number == ai 
+            puts "Correct!"
+        elsif number < ai 
+            puts "Lower."
+        else 
+            puts "Higher."
+
+        until number == ai 
+            if number < ai
+                print "Wrong, guess higher"
+            else 
+                print "Wrong, guess lower"
+                number = gets.to_i
+        end
+
+        puts "Congratulations"
     end
 end
