a23f5a9141a42cfb00442777bb54cd97ad128ed7
Penelope Chen <pen.c828@gmail.com>
2023-01-30 15:22:20 +1100

    copy pasta js file & figuring out how to make book cover appear

--- 10-Penelope-Chen/wk07/1-mon/gg_books/js/gg_books.js
+++ 10-Penelope-Chen/wk07/1-mon/gg_books/js/gg_books.js
@@ -1,1 +1,21 @@
-console.log('yeet')
+const fetchCover = function () {
+    const xhr = new XMLHttpRequest(); 
+        xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:Jaws');
+        xhr.send(); 
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+            return;
+        }
+            const data = JSON.parse( xhr.responseText ); // turn the string into a JS object
+
+            console.log(data);
+
+            const img = document.createElement('img');
+            img.innerText = data.text;
+            document.body.appendChild(img)
+        }
+}
+
+document.getElementById('fetch').addEventListener('click', fetchCover);
+fetchCover(); // First book cover for free
