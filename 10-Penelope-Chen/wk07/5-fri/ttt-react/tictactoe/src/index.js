98d6a0e332669e3529bfaf8fedde512fffdf37bf
Penelope Chen <pen.c828@gmail.com>
2023-02-05 14:31:25 +1100

    finished ttt time travel edition

--- 10-Penelope-Chen/wk07/5-fri/ttt-react/tictactoe/src/index.js
+++ 10-Penelope-Chen/wk07/5-fri/ttt-react/tictactoe/src/index.js
@@ -47,15 +47,18 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
+            history: [
+                {
+                    squares: Array(9).fill(null),
+                }
+            ],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -63,16 +66,26 @@ class Game extends React.Component {
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            history: history.concat([{
-                squares: squares
-            }]),
+            history: history.concat([
+                {
+                    squares: squares
+                }
+            ]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -80,7 +93,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
@@ -124,7 +137,7 @@ function calculateWinner(squares) {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6],
+        [2, 4, 6]
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
@@ -134,31 +147,3 @@ function calculateWinner(squares) {
     }
     return null;
 }
-
-// history = [
-//     // Before 1st move
-//     {
-//         squares: [
-//             null, null, null,
-//             null, null, null,
-//             null, null, null,
-//         ]
-//     },
-//     // After 1st move
-//     {
-//         squares: [
-//             null, null, null,
-//             null, 'X', null,
-//             null, null, null,
-//         ]
-//     },
-//     // After 2nd move
-//     {
-//         squares: [
-//             null, null, null,
-//             null, 'X', null,
-//             null, null, 'O',
-//         ]
-//     },
-//     // ...
-// ]
